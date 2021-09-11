0fb84b342bd57c721af8316d9f228f0a2a9fcb29
LEEGE0 <leegeo9945@gmail.com>
2021-09-11 14:40:12 +0900

    2주차 과제 제출

---
+++ Project1/report2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("학번 : %d \n", 18930079);
+
+	char name[] = "이지오";
+	printf("이름 : %s \n", name);
+
+	char grade = 'A';
+	printf("학점 : %c \n", grade);
+
+		return 0;
+}
