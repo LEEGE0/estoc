fc91c020a84cc270a86b37225ccb1f3b4f7f1a0e
leege0 <leegeo9945@gmail.com>
2021-09-29 18:30:12 +0900

    5주차 실습과제

--- Project4/소스.cpp
+++ Project4/소스.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include<cstdlib>
 #include<ctime>
+#include<math.h>
 #define SIZE 6
 
 int main(void)
@@ -14,15 +15,14 @@ int main(void)
 	printf("주사위를 몇번 던질지 선택하세요 : \n");
 	scanf_s("%d", &a);
 	
-	for (i = 0; i <= a; i++)
-		++freq[rand() % 6];
+	for (i = 0; i <= a; i++) ++freq[rand() % 6];
 	
-	printf("---------------------------------\n");
-	printf(" 눈금수	       해당눈금이나온횟수\n");
-	printf("---------------------------------\n");
+	printf("--------------------------------------------------------\n");
+	printf("\t눈금수	       해당눈금이나온횟수          확률\n");
+	printf("--------------------------------------------------------\n");
 
 	for (i = 0; i < SIZE; i++)
-		printf("%3d	%16d\n", i + 1, freq[i]);
+		printf("\t%3d \t\t\t%d \t\t%.3f%% \n", i + 1, freq[i],(float)freq[i]/a*100);
 
 	return 0;
 }
